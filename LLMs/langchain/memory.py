b1c5f109a10992954fdb59b05c2e20e2e360363b
MinkyoJeong1 <isakin.tyrfing@gmail.com>
2024-02-05 17:05:55 +0900

    REMOVE: remove unnecessary files

--- LLMs/langchain/memory.py
+++
@@ -1,9 +0,0 @@
-from langchain.prompts import MessagesPlaceholder
-from langchain.memory import ConversationBufferWindowMemory
-
-
-agent_kwargs = {
-    "extra_prompt_messages": [MessagesPlaceholder(variable_name="chat_history")],
-}
-# memory = ConversationBufferWindowMemory(memory_key="chat_history", return_messages=True, k=5)
-memory = ConversationBufferWindowMemory(memory_key='history', ai_prefix="AI Assistant", k=3)
--- lib/const.py
+++ lib/const.py
@@ -3,7 +3,7 @@ import json
 
 
 # NPC data
-CHARACTERS = json.load((Path(__file__).resolve().parents[1] / "resources/data/npc_4.json").open('r', encoding='utf-8'))
+CHARACTERS = json.load((Path(__file__).resolve().parents[1] / "resources/data/npc.json").open('r', encoding='utf-8'))
 PLACES = json.load((Path(__file__).resolve().parents[1] / "resources/data/places.json").open('r', encoding='utf-8'))
 
 
