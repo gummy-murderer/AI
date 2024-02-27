e5dad706fda60f4312095b99e45d7f04a5a8b8b7
MinkyoJeong1 <isakin.tyrfing@gmail.com>
2024-02-27 16:32:16 +0900

    FIX: add remove zero width spaces

--- domain/etc/etc_router.py
+++ domain/etc/etc_router.py
@@ -12,8 +12,8 @@ router = APIRouter(
              description="openAI secret key를 확인하는 API입니다.", 
              tags=["etc"])
 async def secret_key_validation(secret_key_schema: etc_schema.SecretKeyValidation):
-
     api_key = check_openai_api_key(secret_key_schema.secretKey)
+
     if not api_key:
         raise HTTPException(status_code=404, detail="Invalid OpenAI API key.")
     
